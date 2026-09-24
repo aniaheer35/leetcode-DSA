221b37916891278fa1526d97a17e6a8c96e64245
aniaheer35 <anilaheer7@gmail.com>
2026-09-24 22:18:43 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (81.08%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -2,17 +2,35 @@ class Solution {
 public:
     int countPartitions(vector<int>& nums) {
         int n = nums.size() ; 
-        vector<int> presum ; 
-        presum.push_back(nums[0]) ; 
-        for( int i = 1 ; i < n ; i++ ){
-             presum.push_back({presum[i-1] + nums[i]}) ; 
+
+        int totalsum = 0 ; 
+        for(int i = 0 ; i < n ; i++ ){
+            totalsum += nums[i] ; 
         }
-        int count = 0 ;
+        
+        if(totalsum%2 != 0 ){
+            return  0 ;
+        }
+        int presum = 0 ; 
+        int ans = 0 ;
         for(int i = 0 ; i < n-1 ; i++ ){
-            if(((presum[n-1] - presum[i]) - presum[i])%2 == 0 ) {
-                count++ ; 
+            presum += nums[i] ; 
+            if(((totalsum-presum)-presum)%2 ==0 ){
+                ans++ ; 
             }
         }
-        return count ; 
+        // vector<int> presum ; 
+        // presum.push_back(nums[0]) ; 
+        // for( int i = 1 ; i < n ; i++ ){
+        //      presum.push_back({presum[i-1] + nums[i]}) ; 
+        // }
+        // int count = 0 ;
+        // for(int i = 0 ; i < n-1 ; i++ ){
+        //     if(((presum[n-1] - presum[i]) - presum[i])%2 == 0 ) {
+        //         count++ ; 
+        //     }
+        // }
+        // return count ; 
+   return  ans ;
      }
 };
